eb1472be8b2ddbe6b30d0657df82d37bd707e9cf
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-11-01 23:55:38 -0400

    Using Dependancy Injection.

--- Persistence/Migrations/20201102031147_teste_2.Designer.cs
+++ Persistence/Migrations/20201102031147_teste_2.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201102031147_teste_2")]
+    partial class teste_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
