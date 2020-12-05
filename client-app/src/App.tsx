cd3e3a182f619b8ad1c3c14c7f000f4daea84d2a
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-12-05 05:57:48 -0400

    Adding the activity entity.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,38 +1,40 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
+import { Header, Icon, List } from 'semantic-ui-react'
 
 class App extends Component {
   state = {
     values: []
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios.get('http://localhost:5000/api/values')
       .then((response) => {
-        console.log(response);
-        this.setState({         
+        this.setState({
           values: response.data
-        }) 
+        })
       })
 
   }
 
-  render(){ 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {this.state.values.map((value: any) => {
-            <li key={value.id}>{value.name}</li>
-          })}
-        </ul>
-      </header>
-    </div>
-  );
- }
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}> {value.name}</List.Item>
+          ))}
+        </List>
+
+      </div>
+    );
+  }
 }
 
 export default App;
