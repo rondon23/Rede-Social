cf8be6ede10b8f4e6bf86b0008665a788fb49745
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-12-16 20:23:20 -0400

    Folder structure in React

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './app/layout/styles.css';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
