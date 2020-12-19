846ea4d017eb254f2e4c66fb0d2707280bb34f0d
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-12-19 18:18:56 -0400

    Selecting an individual activity.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,17 +7,19 @@ import ActivityForm from '../form/ActivityForm'
 import ActivityList from './ActivityList'
 
 interface IProps {
-    activities: IActivity[]
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
 }
 
-export const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
+export const ActivityDashboard: React.FC<IProps> = ({ activities, selectActivity, selectedActivity}) => {
     return (
         <Grid>
             <Grid.Column width={10}>
-                <ActivityList activities={activities}/>                
+                <ActivityList activities={activities} selectActivity={selectActivity}/>                
             </Grid.Column>
             <Grid.Column width={6}>
-                <ActivityDetails/>
+                {selectedActivity && <ActivityDetails activity={selectedActivity}/>}
                 <ActivityForm/>
             </Grid.Column>
         </Grid>
