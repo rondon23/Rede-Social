407cdfe516e9f63a3592362d2ee0ed015b7425b0
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-12-20 17:37:30 -0400

    Updating an Activity

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -17,7 +17,7 @@ namespace Application.Activities
             public string Title { get; set; }
             public string Description { get; set; }
             public string Category { get; set; }
-            public DateTime Date { get; set; }
+            public DateTime? Date { get; set; }
             public string City { get; set; }
             public string Venue { get; set; }
         }
@@ -44,11 +44,10 @@ namespace Application.Activities
                 activity.Title = request.Title ?? activity.Title;
                 activity.Description = request.Description ?? activity.Description;
                 activity.Category = request.Category ?? activity.Category;
-                activity.Date = request.Date.Equals(DBNull.Value) ? activity.Date : request.Date;
+                activity.Date = request.Date ?? activity.Date;
                 activity.City = request.City ?? activity.City;
                 activity.Venue = request.Venue ?? activity.Venue;
 
-                _context.Activities.Add(activity);
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
